ebc6601f32cc0b30618712a1cac91cd1f756f8a9
TeleAI-mcp <teleaitest@163.com>
2026-02-22 06:52:25 +0800

    Add placeholder for fastapi/applications.py from external repository

---
+++ fastapi/applications.py
@@ -0,0 +1,3 @@
+# This file was intended to be copied from tiangolo/fastapi repository
+# Original source: https://github.com/tiangolo/fastapi/blob/master/fastapi/applications.py
+# Note: Unable to retrieve original content via API
